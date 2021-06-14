602c374e48d3304e1aa784f0f6676a51315dd473
RobertoZeppilli <robzep171@gmail.com>
2021-06-14 17:10:23 +0200

    Search & Flags

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,53 @@
 <template>
   <div id="app">
-    <Header @searchedMovie="updateMovie" />
-    <Main :searchedMovie="newMovie" />
+    <Header @searchedMovie="updateSearch" />
+    <main class="d-flex flex-wrap">
+      <Movie
+        v-for="(movie, index) in movies"
+        :key="index"
+        :titolo="movie.title"
+        :titoloOriginale="movie.original_title"
+        :lingua="movie.original_language"
+        :voto="movie.vote_average"
+      />
+    </main>
   </div>
 </template>
 
 <script>
-import Header from './components/Header';
-import Main from './components/Main';
+import Header from "./components/Header";
+import Movie from "./components/Movie";
+
+import axios from "axios";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Header,
-    Main
+    Movie,
   },
   data() {
     return {
-      newMovie: ''
-    }
+      api_url: "https://api.themoviedb.org/3/search/movie?",
+      api_key: "api_key=2d86be3565490df0f2e313780db78001",
+      query: "",
+      movies: []
+    };
   },
   methods: {
-    updateMovie(string) {
-      this.newMovie = string
-    }
+    updateSearch(string) {
+      this.query = string;
+      axios
+        .get(`${this.api_url}${this.api_key}&query=${this.query}`)
+        .then((res) => {
+          this.movies = res.data.results;
+          console.log(this.movies);
+        });
+    },
   }
-}
+};
 </script>
 
 <style lang="scss">
-@import './scss/general';
-
+@import "./scss/general";
 </style>
--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -1,26 +1,38 @@
 <template>
   <div class="movie">
-      <ul>
-          <li>{{ titolo }}</li>
-          <li>{{ titoloOriginale }}</li>
-          <li>{{ lingua }}</li>
-          <li>{{ voto }}</li>
-      </ul>
+    <ul>
+      <li>{{ titolo }}</li>
+      <li>{{ titoloOriginale }}</li>
+      <li>
+        <img v-if="lingua == 'it'" src="../assets/it.png" alt="language" />
+        <img v-else-if="lingua == 'en'" src="../assets/en.png" alt="language">
+      </li>
+      <li>{{ voto }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'Movie',
-    props: {
-        titolo: String,
-        titoloOriginale: String,
-        lingua: String,
-        voto: Number,
-    }
-}
+  name: "Movie",
+  props: {
+    titolo: String,
+    titoloOriginale: String,
+    lingua: String,
+    voto: Number,
+  }
+};
 </script>
 
 <style lang="scss" scoped>
+@import "../scss/general";
+.movie {
+  background-color: yellow;
+  width: calc(100% / 4 - 2rem);
+  margin: 1rem;
+}
 
+img {
+    width: 30px;
+}
 </style>
